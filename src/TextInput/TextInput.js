36e1e505acb5a2ee96ef4d7c38f5a700a9e7b3ba
Keith-Miller <Keith.Miller@kbb.com>
2018-06-25 12:48:31 -0700

    addressing build warnings

--- src/TextInput/TextInput.js
+++ src/TextInput/TextInput.js
@@ -5,11 +5,11 @@ class TextInput extends Component {
     render(){
         return (
             <div className="TextInput">
-                <label for={this.props.name}>{this.props.name}</label>
+                <label htmlFor={this.props.name}>{this.props.name}</label>
                 <input 
                     name={this.props.name} 
                     type="text" 
-                    value={this.props.value} 
+                    value={this.props.value || ""} 
                     onChange={this.props.onChange} />
             </div>
         );
